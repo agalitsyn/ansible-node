34c2e46b3bd0d8bafeb3676c001f008a85bdac7e
Anton Galitsyn <anton.galitsyn@aliexpress.ru>
2021-08-02 16:31:04 +0700

    store more history

--- files/bashrc.sh
+++ files/bashrc.sh
@@ -22,3 +22,22 @@ alias ....="cd ../../.."
 alias .....="cd ../../../.."
 alias ~="cd ~"
 alias -- -="cd -"
+
+# Keep a reasonably long history.
+export HISTSIZE=50000000
+
+# Keep even more history lines inside the file, so we can still look up
+# previous commands without needlessly cluttering the current shell's history.
+export HISTFILESIZE=$HISTSIZE;
+
+# don't put duplicate lines or lines starting with space in the history.
+# See bash(1) for more options
+export HISTCONTROL=ignoredups
+
+# Keep track of the time the commands were executed.
+# The xterm colour escapes require special care when piping; e.g. "| less -R".
+# timestamps for bash history. www.debian-administration.org/users/rossen/weblog/1
+export HISTTIMEFORMAT="${FG_BLUE}${FONT_BOLD}%Y/%m/%d %H:%M:%S${FONT_RESET}  ";
+
+# Make some commands not show up in history
+export HISTIGNORE="ls:ls *:cd:cd -:pwd;exit:date:* --help"
